fcd7f3aa7a0998264fd042b06c260830f473c227
Oliver-Browning <oliverbrowning6@gmail.com>
2025-12-06 13:29:11 -0600

    Tried to fix keyboard read input injection. Added console resize function.

--- Pokemon_Sigma_Sapphire/Code/main.py
+++ Pokemon_Sigma_Sapphire/Code/main.py
@@ -1,5 +1,7 @@
 import keyboard
 import os
+import time
+import msvcrt
 
 
 #Change read file path as needed for your file system
@@ -42,12 +44,45 @@ def create_account():
 def draw(char="-"):
     print(f"{char*30}")#------------------------------")
 
+def console_resize(x, y, char):
+    #os.system('cls')
+    clear()
+    inp = input("Do you need to resize console? Type: (Y/n) > ").lower()
+
+    if inp == "y":
+        #os.system('cls')
+        clear()
+
+        print("For this next step, drag the borders of your console to closely match the block of text without touching")
+        print("Once you are finished, press space to continue")
+
+        inp = input("Are you ready to move on to the next step? Type: (Y/n) > ").lower()
+        if inp == "y":
+            #os.system('cls')
+            clear()
+            line = char * x
+            for i in range(y):
+                print(line)
+        
+            get_key()
+            if key == ' ':
+                pass
+
+
+def clear():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+
 
 
 
 def menu():
     #global key
-    os.system('cls') #This is the command for windows os.system('clear') for mac
+    #os.system('cls') #This is the command for windows os.system('clear') for mac
+    clear()
     draw()
     print("1. Play")
     print("2. Login")
@@ -90,8 +125,14 @@ first = True
 
 
 def get_key():
-    global key 
-    key = keyboard.read_key()
+    global key
+    #key = keyboard.read_key()
+    while True:
+        event = keyboard.read_event()
+        if event.event_type == keyboard.KEY_DOWN:
+            key = event.name
+            return
+    
 
 
 def title_screen():
@@ -109,11 +150,14 @@ def title_screen():
 
 
 def main():
+    #os.system('cls') #Maybe change into its own function where we can check if the system is a mac, and if so change the clear command to the mac one
+    clear()
     while True:
         #print(f"In main loop{__name__}")
         global first
         if first:   #If this is the first time running the game, display the title screen
             title_screen()
+            console_resize(100, 40, "#")
             game()
             first = False
         else:   #If not the first time running the game dont display the title screen
