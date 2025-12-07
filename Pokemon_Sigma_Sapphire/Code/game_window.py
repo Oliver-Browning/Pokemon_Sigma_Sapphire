5cfff9dcef801fd2ed1ac79a700bfb10ef9f003d
shafanoam <amihai.noam@gmail.com>
2025-12-07 16:57:46 -0600

    Added safari button and base for tutorial house

--- Pokemon_Sigma_Sapphire/Code/game_window.py
+++ Pokemon_Sigma_Sapphire/Code/game_window.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import file_IO
 import game_functions
+import safari
 
 def run_game(playerData):
 
@@ -18,9 +19,28 @@ def run_game(playerData):
     print(currentCandies)
 
 
-    # generating the main map frame
+    # this function handles entering the tutorial house!
+    def tutorial_house_enter():
+        mainMapFrame.place_forget()
+        helpHouseFrame.place(x=0, y=0, relwidth=1, relheight=1)
+
+    def tutorial_house_leave():
+        helpHouseFrame.place_forget()
+        mainMapFrame.place(x=0, y=0, relwidth=1, relheight=1)
+
+
+
+
+
+
+
+
+
+
+
+    ##### generating the main map frame
     mainMapFrame = tk.Frame(gameWindow)
-    mainMapFrame.pack(fill="both", expand=True)
+    mainMapFrame.place(x=0, y=0, relwidth=1, relheight=1)
     mapCanvas = tk.Canvas(mainMapFrame, width=800, height=600)
     mapCanvas.pack(fill="both", expand=True)
     map = tk.PhotoImage(file="../Images/map.png")
@@ -28,13 +48,29 @@ def run_game(playerData):
 
     # help house
     helpHouseImage = tk.PhotoImage(file="../Images/help_House.png")
-    helpHouseButton = tk.Button(mainMapFrame, image=helpHouseImage, activebackground="sky blue", relief="flat")
+    helpHouseButton = tk.Button(mainMapFrame, image=helpHouseImage, activebackground="sky blue", relief="flat", command=tutorial_house_enter)
     helpHouseButton.place(x=160, y=435, anchor="center", width=97, height=129)
 
     # arena
     arenaImage = tk.PhotoImage(file="../Images/Arena.png")
     arenaButton = tk.Button(mainMapFrame, image=arenaImage, activebackground="sky blue", relief="flat")
     arenaButton.place(x=640, y=430, anchor="center", width=192, height=130)
+
+    # safari
+    safariButton = tk.Button(mainMapFrame, text="safari", activebackground="sky blue", relief="flat", command=lambda: safari.run_safari(playerData))
+    safariButton.place(relx=0.2, rely=0.2, anchor="center")
+
+
+    ##### generating the tutorial house frame
+    helpHouseFrame = tk.Frame(gameWindow)
+    # helpHouseFrame.place(x=0, y=0, relwidth=1, relheight=1)
+    helpHouseCanvas = tk.Canvas(helpHouseFrame, width=800, height=600)
+    helpHouseCanvas.pack(fill="both", expand=True)
+    helpHouseEntryImage = tk.PhotoImage(file="../Images/mainMenu_Background.png")
+    helpHouseCanvas.create_image(0, 0, image=helpHouseEntryImage, anchor="nw")
+    helpHouseExitButton = tk.Button(helpHouseFrame, text="exit", command=tutorial_house_leave)
+    helpHouseExitButton.place(relx=0.5, rely=0.9, anchor="center")
+
 
 
     gameWindow.mainloop()
